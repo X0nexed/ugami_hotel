c38da2b33bb3bb4f3cd67a9c54fc8252fce3066b
Xonex <prokoptsov.k.v.-imo51@edu.kpi.ua>
2026-04-03 14:40:39 +0300

    fix: move css import to js and fix paths

--- Main.js
+++ Main.js
@@ -1,7 +1,5 @@
-// Импорт стилей
-import './src/css/style.css';
+import './src/css/style.css'
 
-// Как только стили загружены, плавно показываем весь сайт
 setTimeout(() => {
     document.body.style.opacity = '1';
 }, 100); // Задержка в 0.1 секунды
