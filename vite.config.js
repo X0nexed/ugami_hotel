c34ce251f10ebf23d962a398d1f76e465794134f
Xonex <prokoptsov.k.v.-imo51@edu.kpi.ua>
2026-04-03 14:34:51 +0300

    fix: add base path for vite and remove opacity

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+    base: '/ugami_hotel/', // Вот эта строчка спасет деплой на GitHub
     plugins: [
         tailwindcss(),
     ],
