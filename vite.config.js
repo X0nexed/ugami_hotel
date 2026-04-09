75bff95f77418e5ecb0518ce7adfc3a1eee1be5d
Xonex <prokoptsov.k.v.-imo51@edu.kpi.ua>
2026-04-09 12:01:17 +0300

    fix: update base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    base: '/ugami_hotel/', // Вот эта строчка спасет деплой на GitHub
+    base: '/',
     plugins: [
         tailwindcss(),
     ],
